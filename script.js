c9f0094edd57ca32afb8a27c551af01c4d245271
Salman mac <salmanmac168@gmail.com>
2025-11-16 22:17:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+Person.prototype.greet = function () {
+  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
+
+function Employee(name, age, jobTitle) {
+  Person.call(this, name, age); // inherit properties
+  this.jobTitle = jobTitle;
+}
+
+// inherit prototype
+Employee.prototype = Object.create(Person.prototype);
+
+// fix constructor reference
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function () {
+  console.log(
+    `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+  );
+};
 
 // Do not change code below this line
 window.Person = Person;
